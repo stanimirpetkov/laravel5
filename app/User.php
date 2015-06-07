cf35dd9f0415e814c0dafd3fcd4d83b6d3f7ef8a
stanimirpetkov <stan.petkov@live.com>
2015-06-07 16:16:38 +0200

    pri
    pri

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-// this is the final solution
+// primavera
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
