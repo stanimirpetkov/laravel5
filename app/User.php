2b8c73f903d739ba8c195b0d72ecee1e0d527925
stanimirpetkov <stan.petkov@live.com>
2015-06-07 16:35:00 +0200

    laravelnew
    laravelnew

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-// primavera
+// laravelnew branch
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
