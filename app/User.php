5083fddd41481a4781960072cd0b4713e2bda02c
stanimirpetkov <stan.petkov@live.com>
2015-06-07 15:21:42 +0200

    da
    da

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-// pacha
+// this is the final solution
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
